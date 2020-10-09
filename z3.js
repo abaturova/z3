e11a02bc618cadb025b0215c97d4b011023c627a
Margarita <72250478+abaturova@users.noreply.github.com>
2020-10-09 10:49:37 +0300

    Update z3.js

--- z3.js
+++ z3.js
@@ -1,9 +1,9 @@
-let arr = [14, 20, 5, 10, 2, 0, 17, 25]
+let arr = [14, 20, 5, 10, 2, 0, 25, 25]
 let l = arr.length - 1
 console.log(arr)
 
 // 1 Сортировка выбором
-/*let min
+let min
 for(let i=0; i<l;i++){
 	min = i
 	for(let j=i+1; j<l;j++){
@@ -41,29 +41,47 @@ for (let i=1; i<arr.length; i++){
     }
     arr[j] = m; 
 }
-console.log(arr)*/
+console.log(arr)
 
 // 4 Сортировка слиянием
-let arr1 = []
-let arr2 = []
-let mid = arr.length/2
 
-const mergeSort = (arrL,arrR) => {
+const merge = (arrL,arrR) => {
 	let arrS = []
 	let i = j = 0
 	while(i<arrL.length && j<arrR.length){
-		if(arr1[i] < arr2[j]){
-			arrS.push(arr1[i++])
-		}else arrS.push(arr2[j++])
+		if(arrL[i] < arrR[j]){
+			arrS.push(arrL[i++])
+		}else  
+				arrS.push(arrR[j++])
 	}
+	while(i < arrL.length){
+		arrS.push(arrL[i])
+		i++
+	}
+	while(j < arrR.length){
+		arrS.push(arrR[j])
+		j++
+	}
+
 	return arrS
 }
 
-for(let i=0; i<mid;i++){
-	arr1.push(arr[i])
-}
-for(let i=mid; i<arr.length;i++){
-	arr2.push(arr[i])
+const sorting = arr => {
+	let arr1 = []
+	let arr2 = []
+	let mid = arr.length/2
+	if(arr.length === 1){
+		return arr
+	}
+	for(let i=0; i<mid;i++){
+		arr1.push(arr[i])
+	}
+	for(let i=mid; i<arr.length;i++){
+		arr2.push(arr[i])
+	}
+
+	return merge(sorting(arr1),sorting(arr2))
 }
 
-console.log(mergeSort(arr1,arr2))
+
+console.log(sorting(arr))
